7af435674994da90a7bc0df275299a4b0200f8ed
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-31 15:26:09 -0400

    added post route for tag and porduct routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,4 +26,13 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,4 +28,13 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
   module.exports = router;
