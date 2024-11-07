488b65e07b371ece165b8328195f0e77b28ed49c
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-11-07 13:52:47 -0500

    added ability to delete, update, and get user information

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -52,3 +52,59 @@ router.post('/logout', (req, res) => {
     }
   });
 
+router.get('/', async (req, res) => {
+    try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+} catch (err) {
+    res.status(500).json(err);
+}
+  });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this ID!'});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this ID!'});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destory(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!'});
+            return;
+        }
+        res.status(200).json({ message: 'Removed user!'});
+    } catch (err) {
+        res.status(500).json(er);
+    }
+});
+
