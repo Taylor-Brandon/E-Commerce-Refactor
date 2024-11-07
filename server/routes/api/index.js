6c5c4aad97fbb732dea2492654f55b3e0a360399
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-11-07 13:58:01 -0500

    added user routes to api file; added seed info for users

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,10 +3,12 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 const checkoutRoutes = require('./checkout-route');
+const userRoutes = require('./user-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 router.use('/checkout', checkoutRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
